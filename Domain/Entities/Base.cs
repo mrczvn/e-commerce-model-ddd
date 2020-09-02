c8341dfbf53e3c3426c919e3a80ad66b8ee72682
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 15:04:17 -0300

    [F]: Name property created

--- Domain/Entities/Base.cs
+++ Domain/Entities/Base.cs
@@ -7,5 +7,8 @@ namespace Domain.Entities
     {
         [Display(Name = "Código")]
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
     }
 }
