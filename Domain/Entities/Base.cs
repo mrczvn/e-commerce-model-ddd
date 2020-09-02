c0f02f3d82448b4597f40f897c5b32fd66431dc8
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 15:03:08 -0300

    [F]: created Base class and the Id property

---
+++ Domain/Entities/Base.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Notifications;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Base : Notifies
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+    }
+}
