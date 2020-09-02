63c9f8b438048a72e60c77eb3035b45cf3b6cab4
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 15:25:50 -0300

    [F]: Add property created of type Task

---
+++ Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGeneric<TEntity> where TEntity : class
+    {
+        Task Add(TEntity Obj);
+    }
+}
