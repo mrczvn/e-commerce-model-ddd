02882bea1896fbd9e37f4f74cba7d039f0f08547
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 15:08:41 -0300

    [F]: created Product class and the Id property

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Notifications;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("Product")]
+    public class Product : Notifies
+    {
+        [Column("PRD_ID")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+    }
+}
