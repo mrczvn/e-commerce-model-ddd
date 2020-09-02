ed89cce4dc8f9012a50d6623815cbb01b1e877a0
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 12:40:31 -0300

    [F]: Notifies class created and PropertyName property

---
+++ Domain/Notifications/Notifies.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Notifications
+{
+    public class Notifies
+    {
+        public Notifies()
+        {
+        }
+
+        [NotMapped]
+        public string PropertyName { get; set; }
+    }
+}
