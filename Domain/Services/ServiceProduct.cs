d75025bfd9f2a3fafbbf8883e500a6f4b11c552f
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-02 15:48:46 -0300

    [F]: created ServiceProduct class that inherits from the Interface IServiceProduct

---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.InterfaceServices;
+using System;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
