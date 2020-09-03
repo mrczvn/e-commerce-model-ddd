cabbc3a77ab78aa2b5c1462176dcc7f6cd89bd46
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-03 15:33:49 -0300

    [F]: inheritance implemented in the AppProduct class and initialized constructor

---
+++ Application/OpenApp/AppProduct.cs
@@ -0,0 +1,57 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces.InterfaceProduct;
+using Domain.Interfaces.InterfaceServices;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.OpenApp
+{
+    public class AppProduct : IProductApp
+    {
+        private IProduct _IProduct;
+        private IServiceProduct _IServiceProduct;
+
+        public AppProduct(IProduct IProduct, IServiceProduct IServiceProduct)
+        {
+            _IProduct = IProduct;
+            _IServiceProduct = IServiceProduct;
+        }
+
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(Product Obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Product Obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Product Obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
