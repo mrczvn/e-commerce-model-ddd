6e1c3a937cc63bfb371c2c3c32ec2697f7e80958
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-04 11:42:04 -0300

    [F]: RepositoryGenerics class created and IGenerics Interface implemented

---
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Interfaces.Generics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<TEntity> : IGeneric<TEntity> where TEntity : class
+    {
+        public Task Add(TEntity Obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(TEntity Obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity Obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
