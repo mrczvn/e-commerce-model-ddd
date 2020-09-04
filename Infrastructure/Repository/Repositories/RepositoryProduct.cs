e71b1ce4183376eca2d4cd148bff5c279ce18371
Marcos Vinicius <marvn.ants1@gmail.com>
2020-09-04 13:50:31 -0300

    [F]: created RepositoryProduct class that is inherited of the RepositoryGenerics class

---
+++ Infrastructure/Repository/Repositories/RepositoryProduct.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.InterfaceProduct;
+using Infrastructure.Repository.Generics;
+
+namespace Infrastructure.Repository.Repositories
+{
+    public class RepositoryProduct : RepositoryGenerics<Product>, IProduct
+    {
+    }
+}
